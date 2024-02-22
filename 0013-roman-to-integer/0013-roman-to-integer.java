9c7a35473ff0434def34a2b19c76cb30b47ea3c5
ydawon0514 <ydawon0514@gmail.com>
2024-02-22 14:42:46 +0900

    Time: 12 ms (8.87%), Space: 44.8 MB (47.89%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,29 +1,28 @@
 class Solution {  
-  public int romanToInt(String s) {
-        // Map to store romans numerals
-        Map<Character, Integer> romanMap = new HashMap<>();
-        // Fill the map
-        romanMap.put('I', 1);
-        romanMap.put('V', 5);
-        romanMap.put('X', 10);
-        romanMap.put('L', 50);
-        romanMap.put('C', 100);
-        romanMap.put('D', 500);
-        romanMap.put('M', 1000);
-        // Length of the given string
-        int n = s.length();
-        // Variable to store result
-        int num = romanMap.get(s.charAt(n - 1));
-        // Loop for each character from right to left
-        for (int i = n - 2; i >= 0; i--) {
-            // Check if the character at right of current character is
-            // bigger or smaller
-            if (romanMap.get(s.charAt(i)) >= romanMap.get(s.charAt(i + 1))) {
-                num += romanMap.get(s.charAt(i));
-            } else {
-                num -= romanMap.get(s.charAt(i));
-            }
-        }
-        return num;
-    }
+	public static int romanToInt(String s) {
+
+		HashMap<String, Integer> romanMap = new HashMap<String, Integer>();
+		romanMap.put("I", 1);
+		romanMap.put("V", 5);
+		romanMap.put("X", 10);
+		romanMap.put("L", 50);
+		romanMap.put("C", 100);
+		romanMap.put("D", 500);
+		romanMap.put("M", 1000);
+
+		int number = 0;
+
+		String[] sArr = s.split("");
+
+		for(int i=0; i<sArr.length; i++){            
+			if(i < sArr.length-1 &&
+                romanMap.get(sArr[i+1]) > romanMap.get(sArr[i])){
+				number += romanMap.get(sArr[i+1]) - romanMap.get(sArr[i]);
+				i++;
+				continue;
+			}
+			number += romanMap.get(sArr[i]);
+		}
+		return number;
+	}
 }
