c6507f374b39dea6b4e2d42982e13f3a2bd91081
ydawon0514 <ydawon0514@gmail.com>
2024-02-22 14:41:03 +0900

    Time: 7 ms (20.67%), Space: 44.5 MB (75.54%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,27 +1,41 @@
 class Solution {  
 	public static int romanToInt(String s) {
 
-		HashMap<String, Integer> romanMap = new HashMap<String, Integer>();
-		romanMap.put("I", 1);
-		romanMap.put("V", 5);
-		romanMap.put("X", 10);
-		romanMap.put("L", 50);
-		romanMap.put("C", 100);
-		romanMap.put("D", 500);
-		romanMap.put("M", 1000);
+		HashMap<Character, Integer> romanMap = new HashMap<Character, Integer>();
+		romanMap.put('I', 1);
+		romanMap.put('V', 5);
+		romanMap.put('X', 10);
+		romanMap.put('L', 50);
+		romanMap.put('C', 100);
+		romanMap.put('D', 500);
+		romanMap.put('M', 1000);
 
 		int number = 0;
 
-		String[] sArr = s.split("");
+//         int num = romanMap.get(s.charAt(n - 1));
+//         // Loop for each character from right to left
+//         for (int i = n - 2; i >= 0; i--) {
+//             // Check if the character at right of current character is
+//             // bigger or smaller
+//             if (romanMap.get(s.charAt(i)) >= romanMap.get(s.charAt(i + 1))) {
+//                 num += romanMap.get(s.charAt(i));
+//             } else {
+//                 num -= romanMap.get(s.charAt(i));
+//             }
+//         }
+//         return num;        
+            
+        
+        int length = s.length();
 
-		for(int i=0; i<sArr.length; i++){            
-			if(i < sArr.length-1 &&
-                romanMap.get(sArr[i+1]) > romanMap.get(sArr[i])){
-				number += romanMap.get(sArr[i+1]) - romanMap.get(sArr[i]);
+		for(int i=0; i< length; i++){            
+    		if(i < length-1 &&
+				romanMap.get(s.charAt(i+1)) > romanMap.get(s.charAt(i))){
+				number += romanMap.get(s.charAt(i+1)) - romanMap.get(s.charAt(i));
 				i++;
 				continue;
 			}
-			number += romanMap.get(sArr[i]);
+			number += romanMap.get(s.charAt(i));
 		}
 		return number;
 	}
