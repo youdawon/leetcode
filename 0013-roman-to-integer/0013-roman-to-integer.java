d34842db5489acee764e769313e2e25ae67b62af
ydawon0514 <ydawon0514@gmail.com>
2024-02-22 14:43:58 +0900

    Time: 6 ms (38.57%), Space: 45.2 MB (13.33%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,27 +1,26 @@
-class Solution {  
-	public static int romanToInt(String s) {
+class Solution {  	public static int romanToInt(String s) {
 
-		HashMap<String, Integer> romanMap = new HashMap<String, Integer>();
-		romanMap.put("I", 1);
-		romanMap.put("V", 5);
-		romanMap.put("X", 10);
-		romanMap.put("L", 50);
-		romanMap.put("C", 100);
-		romanMap.put("D", 500);
-		romanMap.put("M", 1000);
+		HashMap<Character, Integer> romanMap = new HashMap<Character, Integer>();
+		romanMap.put('I', 1);
+		romanMap.put('V', 5);
+		romanMap.put('X', 10);
+		romanMap.put('L', 50);
+		romanMap.put('C', 100);
+		romanMap.put('D', 500);
+		romanMap.put('M', 1000);
 
 		int number = 0;
 
-		String[] sArr = s.split("");
+        int length = s.length();
 
-		for(int i=0; i<sArr.length; i++){            
-			if(i < sArr.length-1 &&
-                romanMap.get(sArr[i+1]) > romanMap.get(sArr[i])){
-				number += romanMap.get(sArr[i+1]) - romanMap.get(sArr[i]);
+		for(int i=0; i< length; i++){
+			if(i < length-1 &&
+				romanMap.get(s.charAt(i+1)) > romanMap.get(s.charAt(i))){
+				number += romanMap.get(s.charAt(i+1)) - romanMap.get(s.charAt(i));
 				i++;
 				continue;
 			}
-			number += romanMap.get(sArr[i]);
+			number += romanMap.get(s.charAt(i));
 		}
 		return number;
 	}
