c59f0bb6b1a535b94b97ead6be9c7d705f8461df
ydawon0514 <ydawon0514@gmail.com>
2024-02-25 13:35:19 +0900

    Time: 1 ms (78.18%), Space: 44.7 MB (64.47%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -7,18 +7,15 @@ class Solution {
       
      int k = 1;
      
-    for(int i=1; i<nums.length; i++){                      
+    for(int i=1; i<nums.length; i++){                     
+        
+        if(nums[i] < nums[k])
+            break;
+        
       if(nums[i] != nums[i-1]){          
           nums[k] = nums[i];
           k++;                        
-      }
-      //   if(nums[i] < nums[i+1] ){
-      //     nums[k] = nums[i+1];
-      //     k++;              
-      //   } else {
-      //       break;
-      //   }        
-      // }
+      }      
     }         
     return k;
   }
