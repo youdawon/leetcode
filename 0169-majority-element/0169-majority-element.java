9e5fc142546357f1ec5d71bfbb63ec748933a867
ydawon0514 <ydawon0514@gmail.com>
2024-02-25 16:10:19 +0900

    Time: 2 ms (77.53%), Space: 53.7 MB (5.00%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,19 +1,9 @@
 class Solution {
     public int majorityElement(int[] nums) {
     
-		// if(nums.length == 1)            
-		// 	return nums[0];
-
-		//linear time : O(N)
-		//Space : O(1)
-		
-        
         int count = 0;
         int major = 0;
-        
-        /* Boyer Moore 알고리즘 
-         major의 전체 개수가 0 으로 변경될 경우 돌아오는 값을 major로 바꾼다. */
-                
+                        
         for(int num : nums){
             
             if(count == 0){
