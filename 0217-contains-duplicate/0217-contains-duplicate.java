954115034af29f422db096eb863b2eaa3c68f53d
ydawon0514 <ydawon0514@gmail.com>
2024-02-26 15:28:55 +0900

    Time: 8 ms (92.39%), Space: 61 MB (7.07%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,13 +1,12 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        
+
         Set<Integer> set = new HashSet<Integer>();
         
         for(Integer num : nums){
             if(!set.add(num)){
                 return true;
             }
-            set.add(num);
         }             
         return false;
     }
