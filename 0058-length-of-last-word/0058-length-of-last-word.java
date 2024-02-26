feefca5f594364ae02308dc7199a0641e29a0661
ydawon0514 <ydawon0514@gmail.com>
2024-02-26 13:40:48 +0900

    Time: 1 ms (39.41%), Space: 41.6 MB (52.66%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,12 +1,15 @@
 class Solution {
     public int lengthOfLastWord(String s) {
         
-        String[] arr = s.replace("/\s+/g", " ").split(" ");
+        String[] arr = s.split(" ");
         
-        if(arr[arr.length - 1] == " "){
-            return arr[arr.length - 2].length();
-        } else {
-           return arr[arr.length - 1].length();            
+        int max = 0;
+        
+        for(int i=0; i<arr.length; i++){
+            if(arr[i].trim()!= "")
+                max = arr[i].trim().length();
         }
+        
+        return max;
     }
 }
