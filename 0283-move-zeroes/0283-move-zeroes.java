9cbcbea29cbeac83faa91d285dbbbdb4a529ec83
ydawon0514 <ydawon0514@gmail.com>
2024-02-27 15:09:59 +0900

    Time: 3 ms (28.36%), Space: 45.5 MB (95.27%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -4,7 +4,11 @@ class Solution {
         int i=0;
         
         for(int j=0; j<nums.length; j++){
-            if(nums[j] != 0 && nums[i] == 0){
+            
+            if(nums[j] == 0)
+                continue;
+            
+            if(nums[i] == 0){
                 nums[i] = nums[j];
                 nums[j] = 0;
             }
