cb55d7e3f3e935f8d95693a1852e8f4669d1850a
ydawon0514 <ydawon0514@gmail.com>
2024-02-27 15:15:03 +0900

    Time: 3 ms (28.36%), Space: 45.9 MB (79.08%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,18 +1,18 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         
-    int in = 0;
-        int n = nums.length;
-        int i = 0;
-        for(i=0;i<n;i++){
-            if(nums[i]==0){
-                continue;
+        int i=0;
+        
+        for(int j=0; j<nums.length; j++){
+            if(nums[j] != 0 && nums[i] == 0){
+                nums[i] = nums[j];
+                nums[j] = 0;
+                i++;
             }
-            nums[in] = nums[i];
-            in++;
-        }
-        for(int j=n-1;j>=in;j--){
-            nums[j]=0;
+            
+            if(nums[i] != 0){
+                i++;
+            }
         }
     }    
 }
