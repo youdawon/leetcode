261665829b1c34d6904f0c2b9ea6c09e70c0e07a
ydawon0514 <ydawon0514@gmail.com>
2024-03-06 12:58:33 +0900

    Time: 0 ms (100.00%), Space: 41.9 MB (22.68%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -13,21 +13,60 @@
  *     }
  * }
  */
+//     public boolean isSymmetric(TreeNode root) {
+//         if (root == null) {
+//             return true;
+//         }
+//         return isMirror(root.left, root.right);
+//     }
+    
+//     private boolean isMirror(TreeNode node1, TreeNode node2) {
+//         if (node1 == null && node2 == null) {
+//             return true;
+//         }
+//         if (node1 == null || node2 == null) {
+//             return false;
+//         }
+//         return node1.val == node2.val && isMirror(node1.left, node2.right) && isMirror(node1.right, node2.left);
+//     }
+// }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if (root == null) {
-            return true;
-        }
-        return isMirror(root.left, root.right);
+        
+        return isChildSymmetric(root.left, root.right);
     }
     
-    private boolean isMirror(TreeNode node1, TreeNode node2) {
-        if (node1 == null && node2 == null) {
+    public boolean isChildSymmetric(TreeNode node1, TreeNode node2){
+        
+        if(node1 == null && node2 == null){
             return true;
         }
-        if (node1 == null || node2 == null) {
+
+        if(node1 == null || node2 == null){
             return false;
         }
-        return node1.val == node2.val && isMirror(node1.left, node2.right) && isMirror(node1.right, node2.left);
+        
+        if(node1.val != node2.val){
+            return false;
+        }
+        
+        return isChildSymmetric(node1.left, node2.right) && 
+            isChildSymmetric(node1.right, node2.left);
     }
 }
