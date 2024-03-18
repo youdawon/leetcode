1d7e8595bc62111a2f18953056145a24fdb7fe5c
ydawon0514 <ydawon0514@gmail.com>
2024-03-18 14:33:39 +0900

    Time: 0 ms (100.00%), Space: 45.2 MB (10.20%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,20 +4,12 @@ class Solution {
         //Time Complexity : O(N)
         //Space Complexity : O(N)
         
-        for(int i=0; i<nums.length-1; i++){
-            for(int j=i+1; j<nums.length; j++){                
-                if(nums[i] + nums[j] == target)
-                    return new int[]{i, j};
+        for(int i=1; i<nums.length; i++){
+            for(int j=i; j<nums.length; j++){                
+                if(nums[j-i] + nums[j] == target)
+                    return new int[]{j-i, j};
             }
         }
-        
-//         for(int i=1;i<nums.length;i++){
-//             for(int j=i;j<nums.length;j++){
-//                 if(nums[j-i]+nums[j]==target){
-//                     return new int[]{j-i,j};
-//                     }
-//                 }
-//             }        
         
         return null;
     }
