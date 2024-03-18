9d8766205e77b06ac3bc58841d13343d389d3537
ydawon0514 <ydawon0514@gmail.com>
2024-03-18 15:24:40 +0900

    Time: 21 ms (10.37%), Space: 55.2 MB (84.84%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -4,13 +4,21 @@ class Solution {
         //Time Complexity : O(N)
         //Space Complexity : O(N)
         
-        if(nums.length == 1)
-            return false;
+//         if(nums.length == 1)
+//             return false;
             
-        Set<Integer> set = new HashSet<>();
+//         Set<Integer> set = new HashSet<>();
 
-        for(int num : nums){
-            if(!set.add(num))
+//         for(int num : nums){
+//             if(!set.add(num))
+//                 return true;
+//         }
+
+        
+        Arrays.sort(nums);
+        
+        for(int i=0; i<nums.length-1; i++){
+            if(nums[i] == nums[i+1])
                 return true;
         }
         
