a5e93a15ec48de0a3f39d940af823e06ee4b7905
ydawon0514 <ydawon0514@gmail.com>
2024-03-18 15:01:53 +0900

    Time: 7 ms (98.65%), Space: 60.5 MB (22.94%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -8,9 +8,9 @@ class Solution {
             return false;
             
         Set<Integer> set = new HashSet<>();
-        
-        for(int i=0; i<nums.length; i++){            
-            if(!set.add(nums[i]))
+
+        for(int num : nums){
+            if(!set.add(num))
                 return true;
         }
         
