cdcea60a20bd183272e6b0bca403e38462a4e1d8
ydawon0514 <ydawon0514@gmail.com>
2024-03-20 10:47:44 +0900

    Time: 5 ms (61.41%), Space: 44.4 MB (84.48%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,16 +1,19 @@
 class Solution {
+    
+    /**
+    * Time Complexity : O(n)
+    * Space Comlexity : O(n)
+    **/
     public boolean canConstruct(String ransomNote, String magazine) {
         
         HashMap<Character, Integer> map = new HashMap<>();
         
         for(char c : ransomNote.toCharArray()){
             map.put(c, map.getOrDefault(c, 0) + 1);
-        }
+        }        
         
-        
-        for(char c : magazine.toCharArray()){
-            
-            if(map.get(c) != null){
+        for(char c : magazine.toCharArray()){          
+            if(map.containsKey(c)){
                 if(map.get(c) == 1){
                     map.remove(c);
                 } else {
