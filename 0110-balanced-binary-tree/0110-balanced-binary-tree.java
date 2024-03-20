677a9714f67d60d14a1182abebf1d12ed41f3760
ydawon0514 <ydawon0514@gmail.com>
2024-03-20 20:12:14 +0900

    Time: 109 ms (5.00%), Space: 44.6 MB (23.87%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    public boolean isBalanced(TreeNode root) {
+    
+        if(root == null){
+            return true;
+        }            
+
+        boolean leftBalanced = isBalanced(root.left);
+        boolean rightBalanced = isBalanced(root.right);
+                
+        int leftDepth = getDepth(root.left);
+        int rightDepth = getDepth(root.right);       
+        
+        System.out.println(leftDepth);
+        System.out.println(rightDepth);        
+
+        return leftBalanced && rightBalanced && Math.abs(leftDepth-rightDepth) <= 1;
+    }
+    
+    public int getDepth(TreeNode root){
+        
+        if(root == null)
+            return 0;
+        
+        int leftDepth = getDepth(root.left) + 1;
+        int rightDepth = getDepth(root.right) + 1;        
+    
+        return Math.max(leftDepth, rightDepth);        
+    }
+}
