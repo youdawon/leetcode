2cc02bc2cdeef4b5daf886a9de5a1da281be261e
ydawon0514 <ydawon0514@gmail.com>
2024-03-21 13:55:47 +0900

    Time: 12 ms (5.11%), Space: 46.4 MB (5.87%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.java
@@ -0,0 +1,74 @@
+class Solution {
+    public String addBinary(String a, String b) {
+                
+        int i = a.length()-1;
+        int j = b.length()-1;
+        
+        char remain = '0';                
+        String result = "";
+
+        
+        while(i >= 0 || j >= 0){
+
+            if(i >= 0 && j < 0){
+                if(remain == '1'){
+                    if(remain == a.charAt(i)){
+                        result += '0';
+                        remain = '1';
+                    } else {
+                        result += '1';
+                        remain = '0';
+                    }
+                } else {
+                    result += a.charAt(i);
+                }
+            } else if (j >= 0 && i < 0){
+                if(remain == '1'){
+                    if(remain == b.charAt(j)){
+                        result += '0';
+                        remain = '1';
+                    } else {
+                        result += '1';
+                        remain = '0';
+                    }
+                } else {
+                    result += b.charAt(j);
+                }            
+            }else{
+                if(a.charAt(i) + b.charAt(j) == 98){
+
+                    if(remain == '1'){
+                        result += '1';                    
+                    } else {
+                        result += '0';
+                    }
+                    remain = '1';                                
+                } else if(a.charAt(i) + b.charAt(j) == 97){
+                    if(remain == '1'){
+                        result += '0';
+                        remain = '1';
+                    } else {
+                        result += '1';                    
+                        remain = '0';
+                    }
+                } else {                
+                    if(remain == '1'){
+                        result += '1';
+                    } else {
+                        result += '0';
+                    }
+                    remain = '0';
+                }                
+            }
+            i--;
+            j--;
+        }
+
+        
+        if(remain != '0')
+            result += '1';
+        
+            System.out.println(result);                
+        return new StringBuffer(result).reverse().toString();
+    }
+}
