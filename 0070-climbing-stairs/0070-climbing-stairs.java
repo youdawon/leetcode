492b99f66da8c28005659fcce55d217a61e3c263
ydawon0514 <ydawon0514@gmail.com>
2024-03-21 14:24:28 +0900

    Time: 0 ms (100.00%), Space: 40.5 MB (27.99%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -5,22 +5,19 @@ class Solution {
     public int climbStairs(int n) {
        
         if(n <= 1){
-            if(map.get(n) == null){
+            if(map.containsKey(n)){
+                return map.get(n);                                
+            } else {
                 map.put(n, 1);
                 return 1;
-            } else {
-                return map.get(n);                
             }
         }
-
-        if(map.get(n-1) == null){
-            map.put(n-1, climbStairs(n-1));
+        
+        if(map.containsKey(n)){
+            return map.get(n);
+        } else {
+            map.put(n, climbStairs(n-1) + climbStairs(n-2));            
+            return map.get(n);
         }
-
-        if(map.get(n-2) == null){
-            map.put(n-2, climbStairs(n-2));
-        }
-
-        return map.get(n-1) + map.get(n-2);
     }    
 }
