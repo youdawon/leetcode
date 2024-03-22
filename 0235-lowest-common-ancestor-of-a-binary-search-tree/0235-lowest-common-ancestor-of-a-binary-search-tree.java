17c9778edd2e00e78486e8ad965a606b7392407d
ydawon0514 <ydawon0514@gmail.com>
2024-03-22 12:24:41 +0900

    Time: 5 ms (100.00%), Space: 44.4 MB (98.37%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -10,62 +10,15 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+        if(root == null)
+            return null;
 
-        
-        Stack<TreeNode> pStack = new Stack<TreeNode>();
-        Stack<TreeNode> qStack = new Stack<TreeNode>();  
-        
-        pStack = getCommonAncestor(root, p, pStack);
-        qStack = getCommonAncestor(root, q, qStack);        
-        
-        int end = 0;
-        
-        if(pStack.size() > qStack.size()){
-            end = pStack.size();
-        } else {
-            end = qStack.size();            
+        if(root.val > p.val && root.val > q.val){
+            return lowestCommonAncestor(root.left, p, q);
+        } else if(root.val < p.val && root.val < q.val){
+            return lowestCommonAncestor(root.right, p, q);            
         }
-        
-        int i=0; 
-        
-        
-        while(i < end){
- 
-            if(pStack.size() > qStack.size()){
-                pStack.pop();   
-            }
-            if(qStack.size() > pStack.size()){
-                qStack.pop();   
-            }    
-            
-            if(qStack.size() == pStack.size()){
-                if(qStack.peek() == pStack.peek())
-                    return qStack.pop();
-                
-                qStack.pop();
-                pStack.pop();
-            }
-            i++;
-        }
-        
-        return new TreeNode();
-    }
-    
-    public Stack<TreeNode> getCommonAncestor(TreeNode root, TreeNode child, 
-                                    Stack<TreeNode> stack){
-
-        stack.push(root);
-        
-        if(root.val == child.val){
-            return stack;
-        }
-        
-        if(root.val > child.val){
-            stack = getCommonAncestor(root.left, child, stack);
-        } else {
-            stack = getCommonAncestor(root.right, child, stack);            
-        }        
-        
-        return stack;
+        return root;
     }
 }
