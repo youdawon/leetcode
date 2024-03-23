2ebcbd36ce7f6e82ca42e8d11621924feff6122d
ydawon0514 <ydawon0514@gmail.com>
2024-03-23 13:10:39 +0900

    Time: 4 ms (99.72%), Space: 46.2 MB (7.34%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -8,12 +8,12 @@ class Solution {
         int neighbours = 0;
         
         for(int i=0; i<grid.length; i++){
-            for(int j=0; j<grid[0].length; j++){
+            for(int j=0; j<grid[i].length; j++){
                 if(grid[i][j] == 1){
                     land++;
                     if(i+1 < grid.length && grid[i+1][j] == 1)
                         neighbours++;
-                    if(j+1 < grid[0].length && grid[i][j+1] == 1)
+                    if(j+1 < grid[i].length && grid[i][j+1] == 1)
                         neighbours++;                
                 }                    
             }
