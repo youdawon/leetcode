b2f0cc72caf77c45d74f13b66b3a590d7c4e7472
ydawon0514 <ydawon0514@gmail.com>
2024-03-23 11:43:25 +0900

    Time: 20 ms (12.12%), Space: 45 MB (29.90%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        
+        int compareValue = image[sr][sc];
+        
+        return fill(image, sr, sc, color, compareValue);        
+    }
+    
+    public int[][] fill(int[][] image, int sr, int sc, int color, int compareValue) {
+               
+            System.out.println(sr);
+            System.out.println(sc);            
+            System.out.println("=====");                        
+        
+        if(sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length)
+            return image;
+        
+        if(image[sr][sc] == compareValue && image[sr][sc] != color){
+            image[sr][sc] = color;        
+            image = fill(image, sr-1, sc, color, compareValue);
+            image = fill(image, sr+1, sc, color, compareValue);                       
+            image = fill(image, sr, sc-1, color, compareValue);
+            image = fill(image, sr, sc+1, color, compareValue);            
+        }
+        
+        return image;
+    }
+}
