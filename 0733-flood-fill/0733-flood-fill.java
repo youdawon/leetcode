bfad5c0aab56a9b7eaea66459295c7c119249ac7
ydawon0514 <ydawon0514@gmail.com>
2024-03-23 11:53:51 +0900

    Time: 0 ms (100.00%), Space: 44.7 MB (77.91%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -6,19 +6,17 @@ class Solution {
         return fill(image, sr, sc, color, compareColor);        
     }
     
-    public int[][] fill(int[][] image, int sr, int sc, int color, int compareColor) {
+    public int[][] fill(int[][] image, int sr, int sc, int newColor, int compareColor) {
                
-        if(sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length)
+        if(sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length 
+          || image[sr][sc] != compareColor || image[sr][sc] == newColor)
             return image;
         
-        if(image[sr][sc] == compareColor && image[sr][sc] != color){
-            image[sr][sc] = color;        
-            image = fill(image, sr-1, sc, color, compareColor);
-            image = fill(image, sr+1, sc, color, compareColor);                       
-            image = fill(image, sr, sc-1, color, compareColor);
-            image = fill(image, sr, sc+1, color, compareColor);            
-        }
-        
+        image[sr][sc] = newColor;        
+        image = fill(image, sr-1, sc, newColor, compareColor);
+        image = fill(image, sr+1, sc, newColor, compareColor);                     
+        image = fill(image, sr, sc-1, newColor, compareColor);
+        image = fill(image, sr, sc+1, newColor, compareColor);            
         return image;
     }
 }
