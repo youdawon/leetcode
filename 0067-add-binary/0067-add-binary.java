1198af3d3ccd8f6ab9425b45d276d9a90f414df2
ydawon0514 <ydawon0514@gmail.com>
2024-03-23 14:01:35 +0900

    Time: 2 ms (53.03%), Space: 42 MB (79.06%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,42 +1,44 @@
 class Solution {
     public String addBinary(String a, String b) {
-                
-        int i = a.length()-1;
-        int j = b.length()-1;
-        int carry = 0;
-        StringBuilder result = new StringBuilder();
 
+        int i=a.length()-1;
+        int j=b.length()-1;
+        int remain = 0;                            
+        
+        StringBuilder sb = new StringBuilder();
         
         while(i >= 0 || j >= 0){
-            int sum = carry;
+
+            int sum = 0;
+            
+            sum += remain;
             
             if(i >= 0){
-                sum += a.charAt(i) - '0';    
+                sum += a.charAt(i) - '0';                
             }
             
             if(j >= 0){
-                sum += b.charAt(j) - '0';
+                sum += b.charAt(j) - '0';                                
             }
             
-            if(sum == 0 || sum == 1){
-                result.append(sum);
-                carry = 0;
+            if(sum <= 1){
+                sb.append(sum);
+                remain = 0;
             } else if (sum == 2){
-                result.append("0");
-                carry = 1;
+                sb.append("0");
+                remain = 1;
             } else {
-                result.append("1");
-                carry = 1;
-            }            
+                sb.append("1");
+                remain = 1;                            
+            }
             i--;
             j--;
         }
-
         
-        if(carry == 1)
-            result.append("1");
+        if(remain == 1){
+            sb.append("1");
+        }
         
-        
-        return result.reverse().toString();
+        return sb.reverse().toString();
     }
 }
