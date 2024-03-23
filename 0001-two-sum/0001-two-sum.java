e0d516fdc23af2589e2a43355fd998f83f293476
ydawon0514 <ydawon0514@gmail.com>
2024-03-23 16:49:58 +0900

    Time: 5 ms (59.08%), Space: 44.8 MB (43.64%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,7 +4,6 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<>();
         
         for(int i=0; i<nums.length; i++){
-            System.out.println(map.get(target - nums[i]));
             if(map.get(target - nums[i]) != null && i != map.get(target - nums[i])){            
                 return new int[]{i, map.get(target-nums[i])};
             }            
