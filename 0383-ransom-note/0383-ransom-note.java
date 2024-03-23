d9bf429aa1cf0b96bbcb6c922609ac44339726a1
ydawon0514 <ydawon0514@gmail.com>
2024-03-23 15:11:05 +0900

    Time: 1 ms (99.14%), Space: 44.5 MB (83.93%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,6 +1,9 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         
+        if(ransomNote.length() > magazine.length())
+            return false;
+        
         int[] arr = new int[26];        
         
         for(char c : magazine.toCharArray()){
