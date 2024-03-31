5f2e6c481bde246cb997e501e6a75c3e31928638
ydawon0514 <ydawon0514@gmail.com>
2024-03-31 15:10:01 +0900

    Time: 25 ms (74.24%), Space: 42.6 MB (67.46%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -19,31 +19,25 @@ class Node {
 */
 
 class Solution {
-    
+public Node cloneGraph(Node node) {
+        if(node == null) return node;
 
-    
-    public Node cloneGraph(Node node) {
-        
-        if(node == null)
-            return null;
+        HashMap<Node,Node> visited = new HashMap<>();
+        Queue<Node> q = new LinkedList<>();
+        q.add(node);
 
-        HashMap<Node, Node> map = new HashMap<Node, Node>();
-        Queue<Node> queue = new LinkedList<Node>();        
-        
-        queue.add(node);
-        map.put(node, new Node(node.val));
-        
-         while(!queue.isEmpty()){
-             Node curNode = queue.poll();
-                    
-             for(Node neighbor : curNode.neighbors){
-                 if(!map.containsKey(neighbor)){
-                     map.put(neighbor, new Node(neighbor.val));
-                     queue.add(neighbor);                     
-                 }
-                 map.get(curNode).neighbors.add(map.get(neighbor));
-             }             
-         }        
-        return map.get(node);
+        visited.put(node,new Node(node.val, new ArrayList())); 
+
+        while(!q.isEmpty()) {
+            Node n = q.poll();
+            for(Node neighbor : n.neighbors) {
+                if (!visited.containsKey(neighbor)) { 
+                    visited.put(neighbor,new Node(neighbor.val, new ArrayList())); 
+                    q.add(neighbor);
+                }
+                visited.get(n).neighbors.add(visited.get(neighbor));
+            }
+        }
+        return visited.get(node);
     }
 }
