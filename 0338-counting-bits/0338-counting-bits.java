5a27d3cfb5bf5cc50ef829c8a7246425f3bfe6b0
ydawon0514 <ydawon0514@gmail.com>
2024-04-01 14:13:41 +0900

    Time: 2 ms (93.27%), Space: 46.6 MB (33.38%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] countBits(int n) {
+
+        int[] result = new int[n+1];
+        result[0] = 0;        
+ 
+        if(n == 0)
+            return result;
+        
+       for(int i=1; i<=n; i++){
+           int remainder = i % 2;
+           int divided = i / 2;
+           
+           result[i] = result[divided] + remainder;
+       }        
+        
+        return result;
+    }
+}
