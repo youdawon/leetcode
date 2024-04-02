e33652fc99e63806006ba9906f6b93a6dd6019f9
ydawon0514 <ydawon0514@gmail.com>
2024-04-02 14:39:11 +0900

    Time: 39 ms (47.09%), Space: 55.7 MB (13.11%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -11,18 +11,16 @@ class Trie {
         TrieNode current = this.root;
         
         for(int i=0; i<word.length(); i++){
-
-            TrieNode child = new TrieNode();
             
-            if(!current.children.containsKey(word.charAt(i))){
-                child = new TrieNode(word.charAt(i));                
-                current.children.put(word.charAt(i), child);
-            } else {
-                child = current.children.get(word.charAt(i)); 
-            }   
+            TrieNode child = current.children[word.charAt(i) - 'a'];
             
-            if(i == word.length()-1)
-                child.isEndOfWord = true;
+            if(child == null){
+                child = new TrieNode(word.charAt(i));
+                current.children[word.charAt(i) - 'a'] = child;
+            }
+            
+            if(i == word.length() - 1)
+                child.isEndOfWord = true; 
             
             current = child;
         }
@@ -31,33 +29,40 @@ class Trie {
     public boolean search(String word) {
         
         TrieNode current = this.root;
-
-        for(int i=0; i<word.length(); i++){            
-            if(!current.children.containsKey(word.charAt(i)))
+        
+        for(int i=0; i<word.length(); i++){
+            
+            TrieNode child = current.children[word.charAt(i) - 'a'];
+            
+            if(child == null){
                 return false;
+            }
             
-            current = current.children.get(word.charAt(i));  
-            
-
-                            
-            if(i == word.length() - 1 && current.isEndOfWord)
+            if(i == word.length() - 1 && child.isEndOfWord)
                 return true;
+            
+            current = child;
         }
         
         return false;
     }
     
     public boolean startsWith(String prefix) {
+        
         TrieNode current = this.root;
-
+        
         for(int i=0; i<prefix.length(); i++){
-            if(!current.children.containsKey(prefix.charAt(i)))
-                return false;
             
-            current = current.children.get(prefix.charAt(i));            
-                            
+            TrieNode child = current.children[prefix.charAt(i) - 'a'];
+            
+            if(child == null){
+                return false;
+            }
+            
             if(i == prefix.length() - 1)
                 return true;
+            
+            current = child;
         }
         
         return false;
@@ -68,17 +73,17 @@ class TrieNode{
     
     char c;
     boolean isEndOfWord;
-    HashMap<Character, TrieNode> children;
+    TrieNode[] children;
 
     TrieNode(){
         isEndOfWord = false;
-        this.children = new HashMap<Character, TrieNode>();
+        this.children = new TrieNode[26];
     }
         
     TrieNode(char c){
         this.c = c;
         isEndOfWord = false;
-        this.children = new HashMap<Character, TrieNode>();        
+        this.children = new TrieNode[26];
     }
 }
 
