6d6ce1e5445ba7775bc4e895114705abfbd199da
ydawon0514 <ydawon0514@gmail.com>
2024-04-04 09:51:10 +0900

    Time: 21 ms (89.22%), Space: 55.7 MB (5.13%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,60 +1,23 @@
 class Solution {
-    public int[][] kClosest(int[][] points, int k) {
+   public int[][] kClosest(int[][] points, int k) {
         
         if(k == points.length)
             return points;
-        
-        quickSort(points, 0, points.length-1);
-        
-        return Arrays.copyOfRange(points, 0, k);
-    }
-    
-    public void quickSort(int[][] points, int low, int high){
-        
-        if(low >= high)
-            return;
-        
-        int pi = partition(points, low, high);
-        
-        quickSort(points, low, pi-1);
-        quickSort(points, pi+1, high);        
-    }
-    
-    public int partition(int[][] points, int low, int high){
-        
-        int pivotPoint = (low + high) / 2;        
-        int[] pivot = points[pivotPoint];
-        int i=low;
-        int j=high;
-        
-        swap(points, low, pivotPoint);        
-        
-        while(i<j){
-            while(getDistance(pivot) < getDistance(points[j])){
-                j--;
-            }
 
-            while(i<j && getDistance(pivot) >= getDistance(points[i])){
-                i++;
-            }            
-            swap(points, i, j);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((arr1, arr2) -> {
+            int p1 = arr1[0] * arr1[0] + arr1[1] * arr1[1];
+            int p2 = arr2[0] * arr2[0] + arr2[1] * arr2[1];            
             
-        }  
-                
-        points[low] = points[i];
-        points[i] = pivot;  
-
-
-        return i;
-    }    
-    
-    public int getDistance(int[] value){
-        return value[0]*value[0] + value[1]*value[1];
-    }
-    
-    public void swap(int[][] points, int i, int j){
-        int[] temp = points[i];
-        points[i] = points[j];
-        points[j] = temp; 
+            return p2 - p1;
+        });
+        
+        for(int[] point : points){
+            pq.offer(point);
+            
+            if(pq.size() > k)
+                pq.poll();
+        }
+        
+        return pq.toArray(new int[0][0]);
     }
 }
