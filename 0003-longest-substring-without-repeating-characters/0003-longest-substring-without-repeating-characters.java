b022c22e7e4a60726ad8ce7f6193cb90c09eb72a
ydawon0514 <ydawon0514@gmail.com>
2024-04-06 12:43:18 +0900

    Time: 6 ms (69.77%), Space: 44.6 MB (52.97%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -7,19 +7,14 @@ class Solution {
         int compare = 0;
         
         for(int i=0; i<s.length(); i++){
-            System.out.println(start);
             if(map.containsKey(s.charAt(i)) && 
               map.get(s.charAt(i)) >= start){
-                
-                System.out.println("compare " + compare);
-                System.out.println(" result " + result);                
                 if(compare > result){
                     result = compare;                    
                 }
                 start = map.get(s.charAt(i)) + 1;                
                 compare = i - start + 1;
                 map.put(s.charAt(i), i);                
-                System.out.println(" compare compare " + compare);
             } else {        
                 map.put(s.charAt(i), i);
                 compare += 1;
