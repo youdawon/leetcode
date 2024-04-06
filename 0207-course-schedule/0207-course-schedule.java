7565cf43b3b0030b1c4033f650f13683feb5fd2e
ydawon0514 <ydawon0514@gmail.com>
2024-04-06 15:37:35 +0900

    Time: 3 ms (94.75%), Space: 44.3 MB (82.72%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,53 +1,45 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         
-        if(prerequisites.length == 0)
-            return true;
+        List<ArrayList<Integer>> courses = new ArrayList<ArrayList<Integer>>();
         
-        List<List<Integer>> courses = new ArrayList<List<Integer>>();
-        
-        //init map
         for(int i=0; i<numCourses; i++){
-            courses.add(new ArrayList<Integer>());
-        }
-         
-        //setting map
-        for(int[] prerequisity : prerequisites){
-            int preCourse = prerequisity[1];
-            int curCourse = prerequisity[0];            
-            
-            courses.get(preCourse).add(curCourse);
-        }
-        
-        int[] visited = new int[numCourses];
-                
-        for(int key = 0; key < courses.size(); key++){
-            if(isCycling(courses, visited, key)){
-                return false;
-            }
+            courses.add(new ArrayList<Integer>());                        
         }
 
+        for(int[] pre : prerequisites){            
+            int preCourse = pre[1];
+            int curCourse = pre[0];
+            courses.get(preCourse).add(curCourse);            
+        }
+        
+        Queue<Integer> q = new LinkedList<>();
+        int[] visited = new int[numCourses];
+        
+        for(int i=0; i<numCourses; i++){
+            if(isCyclic(courses, i, visited))
+                return false;
+        }
+            
+        
         return true;
     }
     
-    public boolean isCycling(List<List<Integer>> courses, int[] visited, int key){
-        
-        if(visited[key] == 2)
+    public boolean isCyclic(List<ArrayList<Integer>> courses, int number, int[] visited){
+        if(visited[number] == 2)
             return true;
         
-        visited[key] = 2;
+        visited[number] = 2;
         
-        for(int child : courses.get(key)){
-            
-            if(visited[child] != 1){                
-                if(isCycling(courses, visited, child)){
+        for(int child : courses.get(number)){
+            if(visited[child] != 1){
+                if(isCyclic(courses, child, visited))
                     return true;
-                }
             }
         }
-       
-        visited[key] = 1;
-
+        
+        visited[number] = 1;
+        
         return false;
     }
 }
