4e5d8e2e8b75dc093ec120bcdba7634f51d32df7
ydawon0514 <ydawon0514@gmail.com>
2024-04-06 13:48:43 +0900

    Time: 6 ms (74.71%), Space: 45 MB (10.99%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,35 +1,31 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<Integer> stack = new Stack<Integer>();
-
-        int left = 0;
-        int right = 0;        
+        Stack<Integer> stack = new Stack<>();
         
-        for(String token : tokens){
+        for(String token : tokens){            
             if(token.length() == 1 && token.charAt(0) - '0' < 0){
-                right = stack.pop();            
-                left = stack.pop();
-            }
- 
-            switch(token){
-                case "+":
-                    stack.push(left + right);
-                break;
-                case "-":
-                  stack.push(left - right);
-                break;
-                case "*":
-                    stack.push(left * right);
-                break;
-                case "/":
-                    stack.push(left / right);
-                break;
-                default:
-                    stack.push(Integer.parseInt(token));
-                break;
+                int right = stack.pop();
+                int left = stack.pop();
+
+                switch(token){
+                    case "+":
+                        stack.push(left + right);                        
+                    break;
+                    case "-":
+                        stack.push(left - right);                        
+                    break;
+                    case "*":
+                        stack.push(left * right);                        
+                    break;
+                    case "/":
+                        stack.push(left / right);                        
+                    break;                        
+                }
+            } else {
+                stack.push(Integer.parseInt(token));
             }
         }
-        
-        return stack.peek();
+                    
+        return stack.pop();
     }
 }
