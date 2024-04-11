367324711ef8fcd05f3cac4e73509c06a2d35b9e
ydawon0514 <ydawon0514@gmail.com>
2024-04-11 15:07:37 +0900

    Time: 35 ms (77.46%), Space: 16.4 MB (99.63%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
@@ -0,0 +1,15 @@
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        
+        result = 0
+        curSum = 0
+        
+        for g in gain:            
+            curSum = curSum + g
+
+            if curSum > result:
+                result = curSum
+                
+        
+        return result
+            
