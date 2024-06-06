2c001ba78583dbbdf1c5c9eac412e12e577a271b
ydawon0514 <ydawon0514@gmail.com>
2024-06-06 13:40:49 +0900

    Time: 55 ms (56.06%), Space: 16.7 MB (36.21%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -1,20 +1,13 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
 
-        dp = [-1] * len(cost)
-
-        def recursion(n):
-            if n < 0:
-                return 0
-            if n < 2:
-                return cost[n]            
-            if dp[n] != -1:
-                return dp[n]
-
-            dp[n] = cost[n] + min(recursion(n-1), recursion(n-2))
-
-            return dp[n]
-
         n = len(cost)
+        first = cost[0]
+        second = cost[1]
 
-        return min(recursion(n-1), recursion(n-2))
+        for i in range(2, n):
+            curr = cost[i] + min(first, second)
+            first = second
+            second = curr
+        
+        return min(first, second)
