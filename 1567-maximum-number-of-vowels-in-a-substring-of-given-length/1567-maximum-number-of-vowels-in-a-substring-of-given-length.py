310789f407720cc245db1250c7fa45cfedd325d6
ydawon0514 <ydawon0514@gmail.com>
2024-06-17 13:47:06 +0900

    Time: 146 ms (29.15%), Space: 17.1 MB (96.34%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -4,14 +4,15 @@ class Solution:
         res = 0
         vowelCnt = 0
         left = 0
+        vowels = "aeiou"
 
         for right in range(len(s)):
-            if s[right] in "aeiou":            
+            if s[right] in vowels:            
                 vowelCnt += 1
 
             res = max(res, vowelCnt)           
             while right - left + 1 >= k:
-                if s[left] in "aeiou":
+                if s[left] in vowels:
                     vowelCnt -=1
                 left += 1
 
