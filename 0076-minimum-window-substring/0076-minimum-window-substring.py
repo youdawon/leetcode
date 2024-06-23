81456066a72d0d3263b31e15fb7b074d57658a79
ydawon0514 <ydawon0514@gmail.com>
2024-06-23 12:57:29 +0900

    Time: 185 ms (29.31%), Space: 17.3 MB (64.95%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,32 +1,29 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-
-        if len(s) == 0 or len(t) == 0:
+        
+        if len(s) < len(t):
             return ""
 
-        letterCount = collections.defaultdict(int)
-        res = ""
+        minStr = ""
         needCount = len(t)
+        letters = collections.Counter(t)
 
-        for c in t:
-            letterCount[c] = letterCount.get(c, 0) + 1
-        
         left = 0
         for right in range(len(s)):
-            if letterCount[s[right]] > 0:
+            if letters[s[right]] > 0:
                 needCount -= 1
 
-            letterCount[s[right]] -= 1
+            letters[s[right]] -= 1
 
             while needCount == 0:
-                if not res or len(res) > len(s[left:right+1]):
-                    res = s[left:right+1]
-                
-                letterCount[s[left]] += 1
+                if not minStr or len(minStr) > right-left + 1:
+                    minStr = s[left:right+1]
 
-                if letterCount[s[left]] > 0:
+                letters[s[left]] += 1                    
+            
+                if letters[s[left]] > 0:
                     needCount += 1
 
                 left += 1
-        
-        return res
+
+        return minStr
