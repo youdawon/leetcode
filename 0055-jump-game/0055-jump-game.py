e6527585ec3534aa9ded03701331d9c289bd8ee5
ydawon0514 <ydawon0514@gmail.com>
2024-06-30 14:43:02 +0900

    Time: 374 ms (34.38%), Space: 17.8 MB (40.41%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,14 +1,15 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
 
+        if len(nums) == 1:
+            return True
+        
         remainder = 0
 
         for num in nums:
             if remainder < 0:
                 return False
-
-            if num > remainder:
-                remainder = num
+            remainder = max(remainder, num)
             remainder -= 1
 
         return True
