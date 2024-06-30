8558bd5eb041ad33835412e12491d03c21db1cc9
ydawon0514 <ydawon0514@gmail.com>
2024-06-30 11:50:32 +0900

    Time: 40 ms (48.13%), Space: 16.5 MB (40.26%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -3,4 +3,15 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        matrix[:] = zip(*matrix[::-1])
+
+        ## space complexity:O(1)
+        ## time complexity:O(n^2)
+
+        m, n = len(matrix), len(matrix[0])
+
+        for i in range(0, m//2):
+            matrix[~i], matrix[i] = matrix[i], matrix[~i]
+
+        for i in range(m):
+            for j in range(i):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
