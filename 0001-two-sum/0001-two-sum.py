562e93dcec0425bd6873ebac53e25f8eca812bd3
ydawon0514 <ydawon0514@gmail.com>
2024-07-03 12:47:20 +0900

    Time: 58 ms (69.57%), Space: 17.7 MB (48.84%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-
-        prefix = {}
+        
+        numMap = collections.defaultdict(int)
 
         for i in range(len(nums)):
-            if prefix.get(target - nums[i]) is not None:
-                return [i, prefix.get(target - nums[i])]
+            if target - nums[i] in numMap:
+                return [i, numMap[target-nums[i]]]
+            numMap[nums[i]] = i
 
-            prefix[nums[i]] = i
+        return []
