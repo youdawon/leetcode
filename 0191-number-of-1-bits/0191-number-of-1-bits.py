b8b299ce8029ca62c0829ba5620ec5e6edfeb99c
ydawon0514 <ydawon0514@gmail.com>
2024-07-04 18:46:51 +0900

    Time: 22 ms (99.39%), Space: 16.5 MB (68.35%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -1,6 +1,9 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
 
+        #Time Complexity : O(1)
+        #Space Complexity : O(1)
+
         count = 0
         
         while n > 0:
