ecd58426d6f1df02b655443ff24ba1b4637d81be
ydawon0514 <ydawon0514@gmail.com>
2024-07-08 11:54:49 +0900

    Time: 4181 ms (47.48%), Space: 16.7 MB (13.31%) - LeetHub

---
+++ 0079-word-search/0079-word-search.py
@@ -0,0 +1,37 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+
+        m, n = len(board), len(board[0])
+        
+        def searchWord(i, j, index, visited):
+            
+            if index == len(word):
+                return True
+
+            if i < 0 or j < 0 or i >= m or j >= n:
+                return False
+
+            if (i, j) in visited:
+                return False
+
+            if board[i][j] != word[index]:
+                return False
+
+            visited.add((i, j))
+
+            res = (searchWord(i-1, j, index+1, visited) or 
+            searchWord(i+1, j, index+1, visited) or 
+            searchWord(i, j-1, index+1, visited) or 
+            searchWord(i, j+1, index+1, visited))
+
+            visited.remove((i, j))
+
+            return res
+    
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                print(board[i][j])
+                if searchWord(i, j, 0, set()):
+                    return True
+
+        return False
