9dbe8edc667ac167ce7e00d32f4ba6dd762a2ecc
ydawon0514 <ydawon0514@gmail.com>
2024-07-13 16:02:42 +0900

    Time: 84 ms (69.55%), Space: 16.7 MB (27.28%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -3,16 +3,16 @@ class Solution:
 
         maxCount, maxLength = -inf, -inf
         arr = [0]*26
+
         left = 0
-
         for right in range(len(s)):
-            arr[ord(s[right]) - ord('A')] += 1
-            maxCount = max(maxCount, arr[ord(s[right]) - ord('A')])
+            arr[ord(s[right])-ord('A')] += 1
+            maxCount = max(maxCount, arr[ord(s[right])-ord('A')])  
 
-            while right-left+1-maxCount > k:
-                arr[ord(s[left]) - ord('A')] -= 1
-                left += 1              
+            while right-left-maxCount+1 > k:
+                arr[ord(s[left])-ord('A')] -= 1
+                left += 1
             
             maxLength = max(maxLength, right-left+1)
-
+        
         return maxLength
