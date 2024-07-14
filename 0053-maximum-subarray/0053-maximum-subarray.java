2fa76deafd0bb633d4326019fde4b3f3fc8c0244
ydawon0514 <ydawon0514@gmail.com>
2024-07-14 14:43:03 +0900

    Time: 1 ms (99.62%), Space: 57.2 MB (23.72%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,6 +1,9 @@
 class Solution {
     public int maxSubArray(int[] nums) {
 
+        //Time Complexity : O(N)
+        //Space Complexity : O(1)
+
         int currSum = 0;
         int maxSum = Integer.MIN_VALUE;
 
