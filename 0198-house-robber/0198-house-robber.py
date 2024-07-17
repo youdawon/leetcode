f95b03dbe26b50f63e5e11156b638aa02f46529e
ydawon0514 <ydawon0514@gmail.com>
2024-07-17 18:27:27 +0900

    Time: 42 ms (16.81%), Space: 16.6 MB (23.34%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,23 +1,10 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        
+        arr = [0]*(len(nums)+1)
+        
+        for i in range(0, len(nums)):
+            arr[i+1] = max(arr[i], nums[i] + arr[i-1])
 
-        ## time complexity : O(n)
-        ## space complexity : O(n)
 
-        dp = [-1]*len(nums)
-
-        def recursion(i):
-
-            if i < 0:
-                return 0
-            
-            if dp[i] != -1:
-                return dp[i]
-
-            dp[i] = max(recursion(i-1), recursion(i-2) + nums[i])
-
-            return dp[i]
-
-        n = len(nums)-1
-
-        return recursion(n)
+        return max(arr[-1], arr[-2])
