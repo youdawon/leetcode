6b1131a4a83121d3f361416f20dc36234cb2a0a4
ydawon0514 <ydawon0514@gmail.com>
2024-07-17 09:23:05 +0900

    Time: 39 ms (43.93%), Space: 16.7 MB (25.01%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,20 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+
+        res = []
+
+        def getSubSets(current, currIndex):
+
+            if currIndex == len(nums):
+                res.append(current.copy())
+                return
+
+            current.append(nums[currIndex])
+            getSubSets(current, currIndex+1) 
+            current.pop()
+            getSubSets(current, currIndex+1)           
+
+        getSubSets([], 0)
+
+
+        return res
