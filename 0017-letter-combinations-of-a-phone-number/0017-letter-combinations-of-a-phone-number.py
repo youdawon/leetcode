6e1cb1ecdf4e2576c9d5c3e21b651f39e5f7dd3c
ydawon0514 <ydawon0514@gmail.com>
2024-07-19 09:43:28 +0900

    Time: 41 ms (17.55%), Space: 16.4 MB (64.25%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,11 +1,12 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
 
+        res = []
+
         if not digits:
-            return []
+            return res
         
         numMap = {"2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
-        res = []
 
         def dfs(current, i):
 
@@ -13,13 +14,10 @@ class Solution:
                 res.append(current)
                 return
 
-            curString = numMap[digits[i]]
+            letters = numMap[digits[i]]
 
-            for j in range(len(curString)):
-                current += curString[j]
-                dfs(current, i+1)
-                current = current[0:len(current)-1]
-
+            for letter in letters:
+                dfs(current+letter, i+1)
 
         dfs("", 0)
         
