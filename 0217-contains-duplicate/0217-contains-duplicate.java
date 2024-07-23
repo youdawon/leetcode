ac12e015f95c7f2e581e4cc21e340e2a3bb6bf15
ydawon0514 <ydawon0514@gmail.com>
2024-07-23 11:46:32 +0900

    Time: 7 ms (99.23%), Space: 60.7 MB (15.62%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,19 +1,12 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        
-        //Time Complexity : O(N)
-        //Space Complexity : O(N)
-        
-        if(nums.length == 1)
-            return false;
-            
-        Set<Integer> set = new HashSet<>();
+
+        Set<Integer> set = new HashSet<>();        
 
         for(int num : nums){
-            if(!set.add(num))
-                return true;
+            if(!set.add(num)) return true;
         }
-        
+
         return false;
     }
 }
