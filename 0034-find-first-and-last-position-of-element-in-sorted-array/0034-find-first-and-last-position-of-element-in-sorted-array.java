09176b53227d2a0d653068b0aa19819f410f2a77
ydawon0514 <ydawon0514@gmail.com>
2024-07-23 18:37:34 +0900

    Time: 0 ms (100%), Space: 46.3 MB (7.01%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,40 +1,56 @@
 class Solution {
+
+    int target;
+    int[] nums;
+
     public int[] searchRange(int[] nums, int target) {
 
-        int[] res = new int[]{-1, -1};
+        this.target = target;
+        this.nums = nums;
 
         int left = 0;
         int right = nums.length-1;
 
-        while(left <= right){
-            int mid = left + (right-left) / 2;
-
-            if(nums[mid] == target){
-                res[0] = mid;
-                right = mid-1;
-            } else if(nums[mid] < target){
-                left = mid+1;
-            } else {
-                right = mid-1;
-            }
-        }
-
-        left = 0;
-        right = nums.length-1;
-
-        while(left <= right){
-            int mid = left + (right-left) / 2;
-
-            if(nums[mid] == target){
-                res[1] = mid;                
-                left = mid+1;
-            } else if(nums[mid] < target){
-                left = mid+1;
-            } else {
-                right = mid-1;
-            }
-        }
-
-        return res;
+        return new int[]{findMinIndex(left, right), findMaxIndex(left, right)};
     }
+
+    public int findMinIndex(int left, int right){
+
+        int minIndex = -1;
+
+        while(left <= right){
+            int mid = left + (right-left) / 2;
+
+            if(this.nums[mid] == this.target){
+                minIndex = mid;
+                right = mid-1;
+            } else if(this.nums[mid] < this.target){
+                left = mid+1;
+            } else {
+                right = mid-1;
+            }
+        }
+
+        return minIndex;        
+    }
+
+    public int findMaxIndex(int left, int right){
+
+        int maxIndex = -1;
+
+        while(left <= right){
+            int mid = left + (right-left) / 2;
+
+            if(this.nums[mid] == this.target){
+                maxIndex = mid;                
+                left = mid+1;
+            } else if(this.nums[mid] < this.target){
+                left = mid+1;
+            } else {
+                right = mid-1;
+            }
+        }
+
+        return maxIndex;        
+    }    
 }
