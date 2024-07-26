b0512e3334e97f330810bdda76d3dc9eb35eff20
ydawon0514 <ydawon0514@gmail.com>
2024-07-26 19:23:01 +0900

    Time: 41 ms (18.02%), Space: 16.6 MB (9.19%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,23 +1,26 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        
+
         res = []
 
         m, n = len(matrix), len(matrix[0])
-        row_start, col_start = 0, 0
-        row_end, col_end = len(matrix)-1, len(matrix[0])-1
+        row_start, row_end = 0, m-1        
+        col_start, col_end = 0, n-1
 
         while row_start <= row_end and col_start <= col_end:
 
             for j in range(col_start, col_end+1):
                 res.append(matrix[row_start][j])
             row_start += 1
+            
             for i in range(row_start, row_end+1):
                 res.append(matrix[i][col_end])
-            col_end -= 1            
+            col_end -= 1
+
             for j in range(col_end, col_start-1, -1):
                 res.append(matrix[row_end][j])
-            row_end -= 1                        
+            row_end -= 1
+
             for i in range(row_end, row_start-1, -1):
                 res.append(matrix[i][col_start])
             col_start += 1
