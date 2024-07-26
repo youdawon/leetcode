0fab895f46ccbfb2de84140d53898cdd0e549ba9
ydawon0514 <ydawon0514@gmail.com>
2024-07-26 14:20:18 +0900

    Time: 92 ms (57.48%), Space: 16.6 MB (38.38%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,24 +1,22 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        
+
+        m, n = len(board)-1, len(board[0])-1
+
         for row in board:
-            if not self.isValid(row):
+            arr = [ value for value in row if value != "." ]
+            if len(set(arr)) != len(arr):
                 return False
 
         for col in zip(*board):
-            if not self.isValid(col):
-                return False                
-
+            arr = [ value for value in col if value != "." ]
+            if len(set(arr)) != len(arr):
+                return False
+               
         for i in (0, 3, 6):
             for j in (0, 3, 6):
-                square = [board[x][y] for x in range(i, i + 3) for y in range(j, j + 3)]
-                if not self.isValid(square):
-                    return False                    
-        return True
-
-    def isValid(self, unit):
-        unit = [ value for value in unit if value != '.' ]
-        if len(set(unit)) != len(unit):
-            return False
+                arr = [ board[x][y] for x in range(i, i+3) for y in range(j, j+3) if board[x][y] != "." ]
+                if len(set(arr)) != len(arr):
+                    return False                
 
         return True
