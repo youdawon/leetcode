17838ae866387cf3ef20e2ce70f24d8df4f213f4
ydawon0514 <ydawon0514@gmail.com>
2024-07-27 09:37:30 +0900

    Time: 79 ms (94.21%), Space: 20.3 MB (50.18%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -6,7 +6,5 @@ class Solution:
         for s in strs:
             sorted_s = ''.join(sorted(s))
             group[sorted_s] = group.get(sorted_s, []) + [s]
-            # else:
-            #     group[sorted_s].append(s)                
         
         return group.values()
