ca4c9999c2997e812e0cad467c0b81172f5d55b9
ydawon0514 <ydawon0514@gmail.com>
2024-07-27 09:47:36 +0900

    Time: 95 ms (35.83%), Space: 20.1 MB (55.82%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,10 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
 
-        group = collections.defaultdict(int)
+        group = collections.defaultdict(int) 
     
-        for s in strs:
-            sorted_s = ''.join(sorted(s))
+        for s in strs: ## O(N)
+            sorted_s = ''.join(sorted(s)) ## O(k logk)
             group[sorted_s] = group.get(sorted_s, []) + [s]
-        
+
+        ## Time : O(N)
+        ## Space : O(N)
+
         return group.values()
