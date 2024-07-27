5f37271718a21f58dd3a2a6ef1cabac6ef97311e
ydawon0514 <ydawon0514@gmail.com>
2024-07-27 10:49:07 +0900

    Time: 785 ms (57.08%), Space: 16.8 MB (93.39%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,11 +1,14 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        
-        dp = [inf]*(amount+1)
-        dp[0] = 0
 
-        for i in range(1, amount+1):
-            for coin in coins:
+        # Time Complexity : O(N*K)
+        #Space Complexity : O(N)
+        
+        dp = [inf]*(amount+1) #O(N)
+        dp[0] = 0 #O(1)
+
+        for i in range(1, amount+1): #O(N)
+            for coin in coins: #O(K)
                 if i < coin:
                     continue
                 dp[i] = min(dp[i], dp[i-coin]+1)
