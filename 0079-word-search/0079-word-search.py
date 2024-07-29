0183be03e50015dd60df53bfc18bd4dab66bce0b
ydawon0514 <ydawon0514@gmail.com>
2024-07-29 15:10:52 +0900

    Time: 4262 ms (45.01%), Space: 16.5 MB (48.32%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,20 +1,14 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-
-        #Time Complexity : O(m*n*4*len(word))
-        #Space Complexity : O(m*n)
-
-        m, n = len(board), len(board[0])
         
-        def searchWord(i, j, index, visited):
-            
-            if index == len(word):
+        m, n = len(board)-1, len(board[0])-1
+
+        def bfs(i, j, index, visited):   
+
+            if len(word) == index:
                 return True
 
-            if i < 0 or j < 0 or i >= m or j >= n:
-                return False
-
-            if (i, j) in visited:
+            if i < 0 or j < 0 or i > m or j > n or (i, j) in visited:
                 return False
 
             if board[i][j] != word[index]:
@@ -22,19 +16,19 @@ class Solution:
 
             visited.add((i, j))
 
-            res = (searchWord(i-1, j, index+1, visited) or 
-            searchWord(i+1, j, index+1, visited) or 
-            searchWord(i, j-1, index+1, visited) or 
-            searchWord(i, j+1, index+1, visited))
+            res = (bfs(i-1, j, index+1, visited) or 
+            bfs(i+1, j, index+1, visited) or 
+            bfs(i, j-1, index+1, visited) or 
+            bfs(i, j+1, index+1, visited))
 
             visited.remove((i, j))
 
             return res
-    
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                print(board[i][j])
-                if searchWord(i, j, 0, set()):
-                    return True
 
+
+        for i in range(m+1):
+            for j in range(n+1):
+                if bfs(i, j, 0, set()):
+                    return True
+        
         return False
