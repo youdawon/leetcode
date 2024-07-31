ac983ea1b28978aae45e3ecc179c0d6178a29a21
ydawon0514 <ydawon0514@gmail.com>
2024-07-31 18:16:33 +0900

    Time: 12 ms (80.22%), Space: 48.5 MB (48.72%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,29 +1,25 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
 
-        // Time Complexity : O(K log D)
-        // Space Complexity : O(D)
-
-        int[] res = new int[k];
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int num : nums){ 
-            map.put(num, map.getOrDefault(num, 0) + 1); // O(logD)
+        int result[] = new int[k];
+        Map<Integer, Integer> map = new HashMap<>();
+        PriorityQueue<Map.Entry<Integer, Integer>> maxHeap = new PriorityQueue<>((a, b) -> 
+        b.getValue() - a.getValue());
+        
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-
-        PriorityQueue<Map.Entry<Integer, Integer>> maxHeap = new PriorityQueue<>((a, b) -> (b.getValue() - a.getValue()));
 
         for(Map.Entry<Integer, Integer> entry : map.entrySet()){
             maxHeap.add(entry);
         }
 
-        
-        while (k > 0){
+        while(k > 0){
             Map.Entry<Integer, Integer> entry = maxHeap.poll();
-            res[k-1] = entry.getKey();
-            k -= 1;
+            result[k-1] = entry.getKey();
+            k -= 1;            
         }
-        
-        return res;
+
+        return result;
     }
 }
