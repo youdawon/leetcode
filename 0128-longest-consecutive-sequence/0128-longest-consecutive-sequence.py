c8f8c68834fc72acc0221f0ab5b53782afab3d26
ydawon0514 <ydawon0514@gmail.com>
2024-08-05 11:30:22 +0900

    Time: 556 ms (38.42%), Space: 48 MB (5.02%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -5,37 +5,37 @@ class Node:
         self.size = 1
 
 class UnionFind:
-    def find(self, node):
+    def findParent(self, node):
         if node != node.parent:
-            node.parent = self.find(node.parent)
+            return self.findParent(node.parent)
         return node.parent
 
     def union(self, node1, node2):
-        parent1 = self.find(node1)
-        parent2 = self.find(node2)
+        parent1 = self.findParent(node1)
+        parent2 = self.findParent(node2)
 
         if parent1 != parent2:
             parent2.parent = parent1
             parent1.size += parent2.size
-
-        return parent1.size      
-
+        
+        return parent1.size
 
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
 
-        currLength, maxLength = 0, 0
+        maxLength = 0
         nodes = {}
         uf = UnionFind()
 
-        for num in nums:
-            if num not in nodes:
-                nodes[num] = Node(num)
+        for i in range(len(nums)):
+            if nums[i] not in nodes:
+                node = Node(nums[i])
+                nodes[nums[i]] = node
                 currLength = 1
-                if num-1 in nodes:
-                    currLength = uf.union(nodes[num-1], nodes[num])
-                if num+1 in nodes:
-                    currLength = uf.union(nodes[num], nodes[num+1])
+                if nums[i]-1 in nodes:
+                    currLength = uf.union(nodes[nums[i]-1], nodes[nums[i]])
+                if nums[i]+1 in nodes:
+                    currLength = uf.union(nodes[nums[i]], nodes[nums[i]+1])
                 maxLength = max(maxLength, currLength)
-        
+
         return maxLength
