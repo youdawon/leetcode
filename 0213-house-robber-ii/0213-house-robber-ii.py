0c2ed7af7d860f9bb571192b58502aeba64ce42b
DaWon <ydawon0514@gmail.com>
2024-08-10 21:41:59 +0900

    Time: 27 ms (96.48%), Space: 16.4 MB (63.05%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -2,22 +2,18 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
 
         if len(nums) == 1:
-            return nums[0]
+            return nums[0]        
 
         n = len(nums)
+        return max(self.getMaxValue(nums, 0, n-1), self.getMaxValue(nums, 1, n))
 
-        def recursion(nums):
+    def getMaxValue(self, nums, start, end):
 
-            prev1 = 0
-            prev2 = 0
+        prev1, prev2 = 0,0
 
-            for num in nums:
-                temp = prev1
-                prev1 = max(num + prev2, prev1)
-                prev2 = temp
-
-            return max(prev1, prev2)
+        for i in range(start, end):
+            temp = prev1
+            prev1 = max(prev1, prev2 + nums[i])
+            prev2 = temp
         
-        return max(recursion(nums[:-1]), recursion(nums[1:]))
-
-        
+        return max(prev1, prev2)
