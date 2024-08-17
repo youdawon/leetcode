1ad35eeafd490cd4d56fb517ee1852cd013e3cb6
DaWon <ydawon0514@gmail.com>
2024-08-17 12:06:40 +0900

    Time: 214 ms (98.17%), Space: 18.8 MB (77.89%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,24 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-
-        m, n = len(grid), len(grid[0])
-        count = 0
-
-        def changeWaterToTheLand(i, j):
-            
-            if i < 0 or j < 0 or i >= m or j >= n or grid[i][j] != "1":
-                return 
-
-            grid[i][j] = "0"
-            changeWaterToTheLand(i-1, j)
-            changeWaterToTheLand(i+1, j)
-            changeWaterToTheLand(i, j-1)
-            changeWaterToTheLand(i, j+1)                        
         
-        for i in range(m):
-            for j in range(n):
+        count = 0
+        m, n = len(grid)-1, len(grid[0])-1
+
+        def dfs(i, j):
+            if i < 0 or j < 0 or i > m or j > n or grid[i][j] == "0":
+                return 
+                
+            grid[i][j] = "0"
+
+            dfs(i-1, j)
+            dfs(i+1, j)
+            dfs(i, j-1)
+            dfs(i, j+1)
+
+        for i in range(m+1):
+            for j in range(n+1):
                 if grid[i][j] == "1":
+                    dfs(i, j)
                     count += 1
-                    changeWaterToTheLand(i, j)
-    
+            
+
         return count
