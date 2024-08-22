1ec209bf7bff3f5303fcb47dede3f9f26bc861ce
DaWon <ydawon0514@gmail.com>
2024-08-22 13:32:39 +0900

    Time: 0 ms (100%), Space: 44.1 MB (88.24%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -9,31 +9,19 @@
  *     }
  * }
  */
-
-/**
-* Floyd's slow and fast algorithm
-**/
-
-
 public class Solution {
     public boolean hasCycle(ListNode head) {
         
-        if(head == null)
-            return false;
-        
-        ListNode slow = head;
         ListNode fast = head;
-        
-        while(fast.next != null && fast.next.next != null){                       
- 
-            slow = slow.next;             
-            fast = fast.next.next;            
-            
-            if(slow == fast){
-                return true;
-            }
+        ListNode slow = head;
+
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(fast == slow) return true;
         }
-        
+
         return false;
     }
 }
