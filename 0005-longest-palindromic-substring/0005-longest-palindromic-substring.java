a50bbaa874ff3eb2e11ed2f7f68287af5ae96f24
DaWon <ydawon0514@gmail.com>
2024-08-24 10:26:10 +0900

    Time: 16 ms (76.35%), Space: 44.8 MB (60.75%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,35 +1,31 @@
 class Solution {
+
+    String maxString = "";
+    String s;
+    int n;
+
     public String longestPalindrome(String s) {
 
-        String maxString = "";
+        this.s = s;
+        this.n = s.length();
 
-        for(int i=0; i<s.length(); i++){
-            int left = i;
-            int right = left+1;
-
-            while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
-                if(maxString.length() < right-left+1){
-                    maxString = s.substring(left, right+1);
-                }
-                left--;
-                right++;
-            }
+        for(int i = 0; i < n; i++){
+            getMaxString(i, i+1);
         }
 
-        for(int i=0; i<s.length(); i++){
-            int left = i;
-            int right = left;
-
-            while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
-                if(maxString.length() < right-left+1){
-                    maxString = s.substring(left, right+1);
-                }
-                left--;
-                right++;
-            }
+        for(int i = 0; i < n; i++){    
+            getMaxString(i, i);
         }        
 
-        return maxString;
+        return this.maxString;
+    }
+    private void getMaxString(int left, int right){
+        while(left >= 0 && right < this.n && this.s.charAt(left) == this.s.charAt(right)){
+            if(this.maxString.length() < right-left+1){
+                this.maxString = this.s.substring(left, right+1);
+            }
+            left--;
+            right++;
+        }        
     }
 }
-
