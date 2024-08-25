6789fe33e7a4fadea93a7b477ab1d8add278bc7c
DaWon <ydawon0514@gmail.com>
2024-08-25 12:09:12 +0900

    Time: 48 ms (40.3%), Space: 57.1 MB (74.15%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,12 +1,10 @@
-class UnionFind {
-    
-    private int[] parent;
-    private int[] size;
+class UnionFind{
+    int[] parent;
+    int[] size;
 
     public UnionFind(int n){
-
         this.parent = new int[n];
-        this.size = new int[n];
+        this.size = new int[n];        
 
         for(int i=0; i<n; i++){
             this.parent[i] = i;
@@ -15,45 +13,44 @@ class UnionFind {
     }
 
     public int find(int index){
-        if(index != this.parent[index]){
-            return find(this.parent[index]);
-        }
-        return index;
-
+        if(parent[index] != index)
+            parent[index] = find(parent[index]);
+        return parent[index];
     }
 
     public int union(int index1, int index2){
-        int pIndex1 = find(index1);
-        int pIndex2 = find(index2);
-        
-        if(pIndex1 != pIndex2){
-            this.parent[pIndex2] = pIndex1;
-            this.size[pIndex1] += this.size[pIndex2];
+        int parent1 = find(index1);
+        int parent2 = find(index2);
+
+        if(parent1 != parent2){
+            parent[parent2] = parent1;
+            size[parent1] += size[parent2];
         }
 
-        return this.size[pIndex1];
+        return this.size[parent1];
     }
-}
 
+}
 class Solution {
     public int longestConsecutive(int[] nums) {
-        
-        UnionFind uf = new UnionFind(nums.length);
-        int maxLength = 0;
-        int currLength = 0;
-        Map<Integer, Integer> map = new HashMap<>();
 
-        for(int i=0; i<nums.length; i++){
-            if(!map.containsKey(nums[i])){
-                map.put(nums[i], i);
-                currLength = 1;
-                if(map.containsKey(nums[i]-1)){
-                    currLength = uf.union(map.get(nums[i]-1), i);
+        int n = nums.length;
+        UnionFind uf = new UnionFind(n);        
+        int maxLength = 0;
+        Map<Integer, Integer> hashMap = new HashMap<>();
+
+        for(int i = 0; i < n; i++){
+            int length = 0;
+            if(!hashMap.containsKey(nums[i])){
+                hashMap.put(nums[i], i);
+                length = 1;
+                if(hashMap.containsKey(nums[i]-1)){        
+                    length = uf.union(hashMap.get(nums[i]-1), i);
                 }
-                if(map.containsKey(nums[i]+1)){
-                    currLength = uf.union(i, map.get(nums[i]+1));
-                }
-                maxLength = Math.max(maxLength, currLength);
+                if(hashMap.containsKey(nums[i]+1)){        
+                    length = uf.union(i, hashMap.get(nums[i]+1));
+                }            
+                maxLength = Math.max(maxLength, length);
             }
         }
 
