0f2558d5b800dbff754b7cb4a5f616f350fbc0f1
DaWon <ydawon0514@gmail.com>
2024-08-26 11:58:19 +0900

    Time: 2 ms (86.23%), Space: 55.3 MB (66.95%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,25 +1,28 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-                
-        int[] result = new int[nums.length];        
-        result[0] = 1;
-        result[nums.length-1] = 1;                
-        int[] temp = new int[nums.length];        
-        temp[0] = 1;
-        temp[nums.length-1] = 1;
-            
-        for(int i=1; i<nums.length; i++){
-            temp[i] = temp[i-1] * nums[i-1]; 
+
+        int[] prefix = new int[nums.length];
+
+        int prev = 1;
+        for(int i = 0; i < nums.length; i++){
+            if(i == 0){
+                prefix[i] = 1;
+            } else {
+                prefix[i] = prev;
+            }
+            prev = nums[i] * prev;
         }
 
-        for(int j=nums.length-2; j>= 0; j--){
-            result[j] = result[j+1] * nums[j+1];
+        prev = 1;
+        for(int i = nums.length-1; i >= 0; i--){
+            if(i == nums.length-1){
+                prefix[i] = 1 * prefix[i];
+            } else {
+                prefix[i] = prev * prefix[i];
+            }
+            prev = nums[i] * prev;
         }        
-        
-        for(int i=0; i<nums.length; i++){
-            result[i] = temp[i] * result[i]; 
-        }
-                
-        return result;
+
+        return prefix;
     }
 }
