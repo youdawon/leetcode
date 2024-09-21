3e4a5b2c1c055d765c1d4353ba179a9344c9bf2b
DaWon <ydawon0514@gmail.com>
2024-09-21 20:43:14 +0900

    Time: 516 ms (96.36%), Space: 0B (100%) - LeetHub

--- 1811-fix-names-in-a-table/1811-fix-names-in-a-table.sql
+++ 1811-fix-names-in-a-table/1811-fix-names-in-a-table.sql
@@ -1,3 +1,9 @@
-/* Write your PL/SQL query statement below */
-select user_id, concat(upper(substr(name, 1, 1)), lower(substr(name, 2))) as name from Users
-order by user_id;
+# Write your MySQL query statement below
+SELECT 
+    user_id,
+    CONCAT(
+        UPPER(SUBSTRING(name, 1, 1)), 
+        LOWER(SUBSTRING(name, 2, LENGTH(name)))
+        ) AS name
+FROM Users
+ORDER BY user_id ASC;
