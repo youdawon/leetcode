d4fe16eb59e345bcd79ed8135b0cc08bd24796c0
DaWon <ydawon0514@gmail.com>
2024-09-21 23:19:28 +0900

    Time: 609 ms (64.59%), Space: 0B (100%) - LeetHub

--- 2087-confirmation-rate/2087-confirmation-rate.sql
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -3,5 +3,5 @@ SELECT
     s.user_id,
     IFNULL(ROUND((sum(action = 'confirmed')/count(s.user_id)), 2), 0) AS confirmation_rate
 FROM Signups s
-LEFT JOIN Confirmations c on s.user_id = c.user_id
+LEFT JOIN Confirmations c ON s.user_id = c.user_id
 GROUP BY s.user_id;
