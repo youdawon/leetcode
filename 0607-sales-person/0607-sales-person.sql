4a921ec6a5003114408b7ad84d6b29643a656129
DaWon <ydawon0514@gmail.com>
2024-09-22 20:22:09 +0900

    Time: 1259 ms (68.98%), Space: 0B (100%) - LeetHub

---
+++ 0607-sales-person/0607-sales-person.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT 
+    s.name
+FROM Orders o 
+JOIN Company c ON (o.com_id = c.com_id AND c.name = 'RED')
+RIGHT JOIN SalesPerson s ON s.sales_id = o.sales_id
+WHERE order_id IS NULL;
