c78c23ee5c19ef65c0d0d94a567bf2c189e38122
Dawon <ydawon0514@gmail.com>
2025-09-14 20:32:48 +0100

    Time: 15 ms (42.33%), Space: 18.5 MB (21.46%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,22 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-
-        # Time Complexity : O(N)
-        # Space Complexity : O(N)
-
-        if len(s) != len(t):
-            return False
-        
-        letters = {}
-
-        for c in s:
-            letters[c] = letters.get(c, 0) + 1
-
-        for c in t:
-            if c not in letters:
-                return False
-            letters[c] -= 1
-            if letters[c] == 0:
-                del letters[c]
-
-        return True
+        return sorted(s) == sorted(t)
